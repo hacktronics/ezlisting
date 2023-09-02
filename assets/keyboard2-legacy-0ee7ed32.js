4ded8c7fa15d8fdbebddb0d6398fea504e920c7c
Hacktronics India <support@hacktronics.co.in>
2023-09-03 01:43:38 +0530

    new

--- assets/keyboard2-legacy-0ee7ed32.js
+++ assets/keyboard2-legacy-0ee7ed32.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-662825e7.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-ac4d2639.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- assets/swipe-back-legacy-a472ce64.js
+++ assets/swipe-back-legacy-a472ce64.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-662825e7.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
+System.register(["./index-legacy-ac4d2639.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.a,i=t.b}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */

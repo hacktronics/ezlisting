9759f122665591680a1ad913961181577546c13c
Hacktronics India <support@hacktronics.co.in>
2023-09-03 02:13:53 +0530

    new

--- assets/keyboard2-legacy-c1b70fa2.js
+++ assets/keyboard2-legacy-c1b70fa2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ac4d2639.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-0c89fd80.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
